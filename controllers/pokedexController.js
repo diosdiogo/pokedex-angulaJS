3a6448c9d1c7d64cac5daec99c255cf752c07aba
Diogo Cesar <39969711+diosdiogo@users.noreply.github.com>
2022-05-25 23:21:42 -0300

    Lista de pokemon

--- controllers/pokedexController.js
+++ controllers/pokedexController.js
@@ -1,3 +1,24 @@
-app.controller('pokedexController', ['$scope', '$rootScope', '$location', '$http', function ($scope, $rootScope, $location, $http) { 
-  $scope.user = "Diogo Cesar"
+app.controller('pokedexController', ['$scope', '$rootScope', '$location', '$http', 'apiService', function ($scope, $rootScope, $location, $http, apiService) { 
+    $scope.user = "Diogo Cesar"
+    $scope.offset = 0;
+    $scope.limit = 20;
+
+    $scope.listaPokemon = []
+    $scope.pokemon = []
+
+    init = () => {
+        var data = {
+            offset: $scope.offset,
+            limit: $scope.limit
+        }
+        apiService.pokemonList(data, (data)=>{
+            if(data.isValid){
+                $scope.pokemon = data.data;
+                $scope.listaPokemon = data.data.data.results
+                console.log($scope.listaPokemon)
+            }
+        })
+    }
+    
+    init()
  }])
--- services/apiService.js
+++ services/apiService.js
@@ -1,100 +1,20 @@
 app.service('apiService', ['$http', function($http) {
 
-    const API = ""
-    const token = ""
+    const API = "https://pokeapi.co/api/v2/"
 
-
-    //Buscar sorteados
-    this.ramdonUserList = function(callback) {
+    //Buscar Pokemon
+    this.pokemonList = function(data,callback) {
         var req = {
             method: 'GET',
-            url: API + "roulette/ramdonUserList",
+            url: API + `pokemon/?offset=${data.offset}&limit=${data.limit}`,
             headers: {
                 'Content-Type': 'application/json',
                 Accept: 'application/json',
-                Authorization: 'Bearer ' + `${token}`
-
-            },
-
-        }
-        var response = $http(req);
-
-        response.then(function(data) {
-            callback({ isValid: true, data: data })
-        })
-        response.catch((data) => {
-            callback({ isValid: false, msg: "Erro ao requisitar servidor!" })
-        })
-    }
-
-    //buscar patrocinadores para sorteio
-    this.customerOrderList = function(callback) {
-        var req = {
-            method: 'GET',
-            url: API + "roulette/customerOrderList",
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: 'Bearer ' + `${token}`
             },
         }
         var response = $http(req);
 
-        response.then(function(data) {
-            callback({ isValid: true, data: data.data.data })
-        })
-        response.catch((data) => {
-            callback({ isValid: false, msg: "Erro ao requisitar servidor!" })
-        })
-    }
-
-    //registrar telespectadores
-    this.register = function(data, callback) {
-        var req = {
-            method: 'POST',
-            url: API + "roulette/register",
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: 'Bearer ' + `${token}`
-
-            },
-            data: data
-
-        }
-        var response = $http(req);
-
-        response.then(function(data) {
-
-            callback({ isValid: true, data: data })
-        })
-        response.catch((data) => {
-            const rest = data.data;
-            console.log(rest)
-            if (rest.error.email) {
-                callback({ isValid: false, msg: rest.error.email[0] })
-            } else if (rest.error.whatsapp) {
-                callback({ isValid: false, msg: rest.error.whatsapp[0] })
-            }
-        })
-    }
-
-    //Ganhado e premio
-    this.winner = function(data, callback) {
-        var req = {
-            method: 'POST',
-            url: API + "roulette/winner",
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: 'Bearer ' + `${token}`
-
-            },
-            data: data
-
-        }
-        var response = $http(req);
-        response.then(function(data) {
+        response.then((data)=> {
             callback({ isValid: true, data: data })
         })
         response.catch((data) => {
